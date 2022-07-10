535fdc955cffbcf8ce6255813c6622c71227d673
quasifu <brian@cheng.ninja>
2022-07-10 14:39:48 -0500

    adding tests

---
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -0,0 +1,18 @@
+from api.controllers import Delivery
+from django_mock_queries.query import MockSet, MockModel
+
+def test_LotsOfItems():
+  order = MockSet()
+  order.add(MockModel(quantity=5))
+  order.add(MockModel(quantity=5))
+  order.add(MockModel(quantity=5))
+  cost = Delivery.calculate(order,6)
+  assert cost == 7.5
+
+
+def test_LittleItems():
+  order = MockSet()
+  order.add(MockModel(quantity=2))
+  order.add(MockModel(quantity=1))
+  cost = Delivery.calculate(order,3)
+  assert cost == 2.5
