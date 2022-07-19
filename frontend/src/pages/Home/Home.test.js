7020054acee702cd33b11f7536c4b88daf716ae4
quasifu <brian@cheng.ninja>
2022-07-19 07:56:05 -0500

    add negative test

--- frontend/src/pages/Home/Home.test.js
+++ frontend/src/pages/Home/Home.test.js
@@ -38,4 +38,38 @@ describe('Test Home', () => {
     expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
     expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
   });
+
+  test('Negative Test: Test Failed Category call', async () => {
+    const mockGet = jest.spyOn(axios, 'get');
+    mockGet.mockImplementation((url) => {
+      switch (url) {
+        case `${API_URL}/api/category/?format=json`:
+          return Promise.resolve({
+            data: {
+              status: 'fail',
+              data: [
+                {
+                  id: 1,
+                  name: 'Handhelds',
+                  description: "So big, you don't need thumbs.",
+                },
+                {
+                  id: 2,
+                  name: 'Appeteasers',
+                  description: 'Tease the hangry hippo, he get hangrier',
+                },
+              ],
+            },
+          });
+        default:
+          return Promise.resolve({
+            data: {
+              status: 'fail',
+            },
+          });
+      }
+    });
+    render(<Home />);
+    expect(screen.queryByTestId(/category-item/i)).not.toBeInTheDocument();
+  });
 });
