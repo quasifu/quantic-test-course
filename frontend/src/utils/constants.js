eaa02cf6d3d555edcc17377bc5c542171a7755f7
quasifu <brian@cheng.ninja>
2022-08-11 18:14:29 -0500

    Revert "test actions"
    This reverts commit 563590c85d308040b51c0dd96a30b962da7d36a9.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,1 @@
-//export const API_URL = 'http://hangry-api-dev.us-east-1.elasticbeanstalk.com';
-export const API_URL = 'http://localhost:8000'
+export const API_URL = 'http://hangry-api-dev.us-east-1.elasticbeanstalk.com';
